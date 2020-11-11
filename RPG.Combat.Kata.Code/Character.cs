fdaa64c58a374dfe14524fe2bb90188ba605eba6
James Golding <69616595+JamesGolding-myob@users.noreply.github.com>
2020-11-11 15:04:22 +1100

    created constants class

--- RPG.Combat.Kata.Code/Character.cs
+++ RPG.Combat.Kata.Code/Character.cs
@@ -2,16 +2,15 @@
 
 namespace RPG.Combat.Kata
 {
+
     public class Character : IHealthChanger
     {
         public int Health{get; private set;}
         public int Level{get; private set;}
         public bool IsAlive => Health > 0;
-        private const int HealingThreshold = 900;
-        private const int DamageAmount = 600;
-        private const int HealAmount = 100;
 
-        public Character(int health = 1000, int level = 1)
+
+        public Character(int health = ImportantValues.MaxHealth, int level = 1)
         {
             Health = health;
             Level = level;        
@@ -19,55 +18,48 @@ namespace RPG.Combat.Kata
 
         public void TakeAction(Action action, IHealthChanger target)//character is currently having too much influence on other Characters
        {
-           if(isValidAttack(action, target))
+           if(IsValidAttack(action, target))
            {   
-               var damageToInflict = AdjustDamageBasedOnCharacterlevelDifference(DamageAmount, this.Level, target.Level);
-               if(Math.Abs(damageToInflict) >= target.Health)
-               {
-                   damageToInflict = - (target.Health);
-               }
+               var damageToInflict = AdjustDamageBasedOnCharacterlevelDifference(ImportantValues.DamageAmount, this.Level, target.Level);
+
                target.ChangeHealth(damageToInflict);
            }
-           else if(isValidHeal(action, target))
+           else if(IsValidHeal(action, target))
            {
-               target.ChangeHealth(HealAmount);
+               target.ChangeHealth(ImportantValues.HealAmount);
            }
        }
 
         public void ChangeHealth(int amountToChange)
-        {   
-            Health = Health + amountToChange;
-            if(Health <= 0)
-            {
-                Health = 0;;
-            }      
+        {       
+            Health = Math.Clamp(Health + amountToChange, ImportantValues.MinHealth, ImportantValues.MaxHealth);        
         }
  
-
         private int AdjustDamageBasedOnCharacterlevelDifference(int damage, int attackerLevel, int targetLevel)
         {
-            int finalDamage = - (damage);
-            if(targetLevel >= (attackerLevel + 5))
+            int finalDamage = - damage;
+            if(targetLevel >= (attackerLevel + ImportantValues.LevelDifference))
             {
                 finalDamage = finalDamage / 2;
             }
-            else if(targetLevel <= (attackerLevel -5))
+            else if(targetLevel <= (attackerLevel - ImportantValues.LevelDifference))
             {
-                finalDamage = finalDamage - 300;
+                finalDamage = ImportantValues.LessenedDamage;
             }
 
             return finalDamage;
         }
 
-        private bool isValidHeal(Action action, IHealthChanger target)
+        private bool IsValidHeal(Action action, IHealthChanger target)
         {
-            return(action == Action.Heal && target == this && this.IsAlive && Health <= HealingThreshold);      
+            return(action == Action.Heal && target == this && this.IsAlive);      
         }
 
-        private bool isValidAttack(Action action, IHealthChanger target)
+        private bool IsValidAttack(Action action, IHealthChanger target)
         {
             return action == Action.Attack && target != this;        
         }
+
 
     }
 }
