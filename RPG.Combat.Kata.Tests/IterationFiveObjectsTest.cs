37f5ebca9aead023eac236fee907eac889d28140
James Golding <69616595+JamesGolding-myob@users.noreply.github.com>
2020-11-17 16:23:19 +1100

    Added Object abstract class and Tree Class

---
+++ RPG.Combat.Kata.Tests/IterationFiveObjectsTest.cs
@@ -0,0 +1,37 @@
+using Xunit;
+using RPG.Combat.Kata;
+namespace RPG.Combat.Kata.Tests
+{
+    public class IterationFiveObjectsTest
+    {
+        [Fact]
+        public void CharactersCanAttackInanimateObjectsLikeTrees()
+        {
+            var tree = new Tree();
+            var woodCutter = new Character();
+
+            woodCutter.TakeAction(ActionType.Attack, tree, true);
+
+            Assert.Equal(1400, tree.Health);
+        }
+
+        [Fact]
+        public void ObjectsCanGetDestroyedWhenHealthReachesZero()
+        {
+            var tree = new Tree(0);
+
+            Assert.True(tree.IsDestroyed);
+        }
+
+        [Fact]
+        public void ObjectsCanNotBeHealedByCharacters()
+        {
+            var tree = new Tree(health:500);
+            var forester = new MeleeCharacter();
+
+            forester.TakeAction(ActionType.Heal, tree, true);
+
+            Assert.Equal(500, tree.Health);
+        }
+    }
+}
