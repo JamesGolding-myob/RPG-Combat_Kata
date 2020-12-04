720583689230ca87eb2e163311a1fb7f53cd3367
James Golding <69616595+JamesGolding-myob@users.noreply.github.com>
2020-12-04 13:39:51 +1100

    Added Input Validation Tests for choosing target

--- RPG.Combat.Kata.Code/Game.cs
+++ RPG.Combat.Kata.Code/Game.cs
@@ -21,63 +21,68 @@ namespace RPG.Combat.Kata
 
         public void Run()
         {
-            UI.DisplayToUser(DisplayConstants.characterSelectionQuestion);
             string input;
             do
             {
+                UI.DisplayToUser(DisplayConstants.characterSelectionQuestion);
                 input = UI.GetResponseFromUser();
+
             } while (!_inputValidator.IsValidCharacterChoice(input));        
                 
             Character character = characterCreator.CreateCharacter(_inputConverter.ConvertCharacterChoice(input), _gameWorld);     
-            
             Monster monster = new Monster(_gameWorld);
+
             SetGameObjectsInWorld(character, monster);
             PerformCombat(character, monster);
         }
 
         public void PerformCombat(Character character, Monster monster)
         {
-            UI.DisplayToUser("you have been sent to kill the monster terrorising the country side");
+            UI.DisplayToUser(DisplayConstants.combatIntroduction);
             do
             {
+                string actionChoice;
                 UI.DisplayToUser(_displayFormater.FormatMap(_gameWorld));
-                UI.DisplayToUser("Choose an Action 1: Attack, 2: Heal, 3: Move Up, 4: Move Right, 5: Move Down, 6: Move Left");
+                UI.DisplayToUser(DisplayConstants.actionChoices);
 
-                var potentialTargets = _gameWorld.GetPotentialTargetsForCharacter(character);
-                var chosenAction = _inputConverter.ActionsConverter(UI.GetResponseFromUser());
+                    do
+                    {
+                        actionChoice = UI.GetResponseFromUser();
 
+                    } while (_inputValidator.ActionChoiceIsInvalid(actionChoice));
+
+                Actions chosenAction = _inputConverter.ActionsConverter(actionChoice);
+                
                 if(chosenAction == Actions.Attack  || chosenAction == Actions.Heal)
                 {
-                    UI.DisplayToUser($"Choose a Target: 1:\n{potentialTargets[0]}\n 2:{potentialTargets[1]}\n 3:{potentialTargets[2]}\n 4:{potentialTargets[3]}\n" );
+                    var potentialTargets = _gameWorld.GetPotentialTargetsForCharacter(character);
                      
-                    character.TakeAction(chosenAction, _inputConverter.ConvertTarget(UI.GetResponseFromUser(), potentialTargets));
+                    UI.DisplayToUser($"Choose a Target: 1:\n{potentialTargets[0]}\n 2:{potentialTargets[1]}\n 3:{potentialTargets[2]}\n 4:{potentialTargets[3]}\n" );
+                    character.TakeAction(chosenAction, _inputConverter.ConvertTarget(actionChoice, potentialTargets));
                 }
                 else
                 {
                     character.TakeAction(chosenAction);
                 }
-
-                
+  
                 UI.DisplayToUser(_displayFormater.ActionFeedback(chosenAction));
                     
                 UI.DisplayToUser(_displayFormater.FormatMap(_gameWorld));
                 if(!monster.IsAlive)
                 {
-                    UI.DisplayToUser("Congratulations you have killed the monster");
+                    UI.DisplayToUser(DisplayConstants.killedMonster);
                     break;
                 }
                 monster.TakeTurn();
-                UI.DisplayToUser("monster Moved");
+                UI.DisplayToUser(DisplayConstants.monsterMoved);
 
             }while(character.IsAlive || monster.IsAlive);
               
         }
 
         private void SetGameObjectsInWorld(Character chosenCharacter, Monster monster)
-        {
-            
-            _gameWorld.SetWorldObjectPosition(4, 4, monster);
-                
+        {  
+            _gameWorld.SetWorldObjectPosition(4, 4, monster);      
             _gameWorld.SetWorldObjectPosition(0, 0, chosenCharacter);
         }
     }
